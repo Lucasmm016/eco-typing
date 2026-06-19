79db8e20051ff9358bf666ebd744bd1cccc07673
Lucas <lucasmm016@gmail.com>
2026-06-19 16:56:48 -0300

    style: adiciona referência para o repositório do projeto no github.

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,24 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
 	return (
 		<main className="w-full max-w-6xl flex flex-col flex-1 mx-auto p-4">
-			<div className="w-full flex flex-col flex-1 items-center justify-center gap-2 mx-auto">
+			<div className="w-full flex flex-col flex-1 items-center justify-center gap-10 mx-auto">
 				<Logo className="w-52" />
 				<div className="space-y-2 max-w-xl text-center text-muted-foreground">
 					<p>Treine inglês digitando. Você digita um texto em inglês trecho a trecho</p>
 					<p>
 						a cada palavra concluída, uma voz fala a pronúncia em inglês e, ao terminar cada trecho,
 						você ouve a tradução em português.
+					</p>
+					<p>
+						Encontre o <b>código fonte</b> deste projeto no{' '}
+						<a
+							href="https://github.com/Lucasmm016/eco-typing.git"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="text-blue-500 underline"
+						>
+							Github
+						</a>
 					</p>
 				</div>
 				<Link href="/typing" className="w-full max-w-sm">
